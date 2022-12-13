a3097b5343d560dfb684bc888614813e504942f4
AHMED HASSAN <118650161+Ahmedkaafi6@users.noreply.github.com>
2022-12-13 10:40:10 +0300

    Update writeNameinaRow.c

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,4 +1,5 @@
 // Online C compiler to run C program online
+
 #include <stdio.h>
 int main(){
  int i,j,k,n; 
@@ -6,12 +7,13 @@ int main(){
  scanf("%d",&n);
  
  for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
     
+     for(j=0;j<n;j++){
+      
+      printf("Akif\t");
+ }
+    printf("\n");
+   
+}
+return 0;
 }
